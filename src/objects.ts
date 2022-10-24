5decd6a7cf548f3272e11ca4275fd8530acf0487
mmfazli <mohammed.fazli@maine.edu>
2022-10-24 18:07:04 -0400

    Quesiton rename complete

--- src/objects.ts
+++ src/objects.ts
@@ -92,6 +92,7 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
+    question.name = newName;
     return question;
 }
 
