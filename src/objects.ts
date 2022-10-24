6ce68bd2ab7c284965cc927d5af749b9d5accd83
mmfazli <mohammed.fazli@maine.edu>
2022-10-24 17:53:01 -0400

    is Correct complete

--- src/objects.ts
+++ src/objects.ts
@@ -30,7 +30,11 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
-    return false;
+    if (question.expected.toLowerCase().trim() == answer.toLowerCase().trim()) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
