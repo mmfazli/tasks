5c15f783745ea9f8c23bbe8fb770851c29c178d3
mmfazli <mohammed.fazli@maine.edu>
2022-10-24 16:50:45 -0400

    makeblankquestion complete

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,10 @@ function App(): JSX.Element {
     return (
         <div className="App COS420">
             <header className="App-header">
-<<<<<<< HEAD
-                HEAD COS420 with React Hooks and TypeScript with React Hooks and
-                TypeScript upstream/task-functions COS420 UM COS420 with React
-                Hooks and TypeScript upstream/task-arrays
-=======
-                UM COS420 with React Hooks and TypeScript
->>>>>>> upstream/task-objects
+                HEAD HEAD COS420 with React Hooks and TypeScript with React
+                Hooks and TypeScript upstream/task-functions COS420 UM COS420
+                with React Hooks and TypeScript upstream/task-arrays UM COS420
+                with React Hooks and TypeScript upstream/task-objects
             </header>
             <h1>Mohammed Fazli</h1>
             <ul>
